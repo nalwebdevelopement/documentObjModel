913d0e3b26a80c6e8fc2e317211eb0bbd07533b9
nalwebdevelopement <naliniu91@gmail.com>
2024-08-11 13:25:21 -0400

    cache element by id

---
+++ app.js
@@ -0,0 +1,10 @@
+// Get the heading element by id 
+
+const idhead = document.getElementById("heading");
+console.log(idhead);
+idhead.textContent = "Document Object Model";
+idhead.style.color = "Black";
+idhead.style.margin = "10px";
+idhead.style.textAlign = "center";
+
+document.append(idhead);
