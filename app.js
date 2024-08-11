8d313eb52e862f45a43f0307828eb2014059e41b
nalwebdevelopement <naliniu91@gmail.com>
2024-08-11 17:23:06 -0400

    CreateElement For the table

--- app.js
+++ app.js
@@ -7,4 +7,91 @@ idhead.style.color = "Black";
 idhead.style.margin = "10px";
 idhead.style.textAlign = "center";
 
-document.append(idhead);
+
+//const value1 = document.querySelector("text");
+let v;
+let v1;
+function putvalue()
+{
+   
+    const value1 = document.getElementById("fname");
+     v = value1.value;
+     const value2 = document.getElementById("lname");
+     v1 = value2.value;
+    console.log(v);
+    
+   // console.log(value.value);
+   // alert("fdsfsdf");
+}
+document.getElementById("submit").addEventListener("click", todo );
+
+
+// create a dynamic table ----------------------------------------
+function createtable()
+{
+const rows = 10;
+const cols = 3;
+
+const app = document.getElementById("table");
+const table = document.createElement("tablebody");
+
+for (let r = 0; r < rows; r++) {
+  const tr = document.createElement("tr");
+  for (let c = 0; c < cols; c++) {
+    const td = document.createElement("td");
+    let x = document.createTextNode(v);
+    
+    td.appendChild(x);
+    tr.appendChild(td)
+    tr.appendChild(td);
+  }
+  table.appendChild(tr);
+
+}
+
+app.appendChild(table);
+}
+// create a dynamic table ----------------------------------------
+
+//call the two fucntions-------------------------------------------
+function todo()
+{
+    putvalue();
+    createtable1();
+
+}
+//call the two fucntions-------------------------------------------
+
+
+
+
+function createtable1()
+{
+
+const app = document.getElementById("table");
+const table = document.createElement("tablebody");
+const tr = document.createElement("tr");
+//const td = document.createElement("td");
+        //let x = document.createTextNode(v);
+        // for (let c = 0; c < 2; c++) {
+        //     const td = document.createElement("td");
+        //     let x = document.createTextNode(v);
+        //     td.appendChild(x);
+        //     tr.appendChild(td);
+        // }
+//tr.appendChild(td)
+const td = document.createElement("td");
+let x = document.createTextNode(v);
+td.appendChild(x);
+tr.appendChild(td);
+const td1 = document.createElement("td");
+let x1 = document.createTextNode(v1);
+td1.appendChild(x1);
+tr.appendChild(td1);
+
+ table.appendChild(tr);
+app.appendChild(table);
+}
+
+
+
